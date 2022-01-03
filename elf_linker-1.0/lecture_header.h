122b949c8cac48f8901a8d3f56ee38be912ecd7b
Girafepothame <45842897+Girafepothame@users.noreply.github.com>
2022-01-03 16:35:33 +0100

    changement structure code

--- elf_linker-1.0/lecture_header.h
+++ elf_linker-1.0/lecture_header.h
@@ -4,27 +4,28 @@
 typedef struct Header Header;
 struct Header
 {
-    char *Magic;
-    char *Class;
-    char *Data;
-    char *Version;
-    char *OS_ABI;
-    char *ABI_V;
-    char *Type;
-    char *Machine;
-    char *Version_Machine;
-    char *Entry_point_adress;
-    char *Start_of_program_headers;
-    char *Start_of_section_headers;
-    char *Flags;
-    char *Size_of_this_header;
-    char *Size_of_program_header;
-    char *Number_of_headers;
-    char *Number_of_program_headers;
-    char *Size_of_section_headers;
-    char *Number_of_section_headers;
-    char *Section_header_string_table_index;
+    char Magic[4];
+    char Class;
+    char Data;
+    char Version;
+    char Pad[9];
 
+    // char *OS_ABI;
+    // char *ABI_V;
+    // char *Type;
+    // char *Machine;
+    // char *Version_Machine;
+    // char *Entry_point_adress;
+    // char *Start_of_program_headers;
+    // char *Start_of_section_headers;
+    // char *Flags;
+    // char *Size_of_this_header;
+    // char *Size_of_program_header;
+    // char *Number_of_headers;
+    // char *Number_of_program_headers;
+    // char *Size_of_section_headers;
+    // char *Number_of_section_headers;
+    // char *Section_header_string_table_index;
 };
 
 
